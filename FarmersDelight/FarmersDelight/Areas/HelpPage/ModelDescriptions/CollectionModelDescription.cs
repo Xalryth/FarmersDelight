d38b5a44d64c2575b826d2868db90d6aed6e0abb
Xalryth <30471411+Xalryth@users.noreply.github.com>
2018-10-24 09:20:18 +0200

    First

---
+++ FarmersDelight/FarmersDelight/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace FarmersDelight.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ FarmersDelight/FarmersDelight/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FarmersDelight.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
